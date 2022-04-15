b51f5a9ee7a2277ecef71711c73825be45e7fe20
raulrocha2 <raul.rocha2@gmail.com>
2022-04-15 09:17:08 -0300

    refactor: create interface Controller

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -1,8 +1,9 @@
 import { MissingParamError } from "../error/MissingParamError"
-import { badRequest } from "../helprs/httpHelper"
+import { badRequest } from "../helpers/httpHelper"
 import { IHttpRequest, IHttpResponse } from "../protocols/http"
+import { IController } from "../protocols/IController"
 
-export class SignUpController {
+export class SignUpController implements IController {
   handle(httpRequest: IHttpRequest): IHttpResponse {
     const requiredField = ['name', 'email', 'password', 'passwordConfirm']
     for (const field of requiredField) {
