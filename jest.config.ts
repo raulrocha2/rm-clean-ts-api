ee01f26294255d27a1a441e3baa0e909be4d3fbb
raulrocha2 <raul.rocha2@gmail.com>
2022-04-15 07:49:31 -0300

    chore: update lintstagedrc.json

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,9 @@
 
 export default {
+  bail: true,
+  coverageProvider: "v8",
   roots: ['<rootDir>/src'],
+  preset: "ts-jest",
   clearMocks: true,
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
