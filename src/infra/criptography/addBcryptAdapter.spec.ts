bd3e44a30ddcb88abf40266a0a030655f83fcf99
raulrocha2 <raul.rocha2@gmail.com>
2022-04-22 13:16:44 -0300

    refactor: move sut create to makeSut function

--- src/infra/criptography/addBcryptAdapter.spec.ts
+++ src/infra/criptography/addBcryptAdapter.spec.ts
@@ -2,24 +2,28 @@ import bcrypt, { hash } from 'bcrypt'
 import { resolve } from 'path'
 import { BcryptAdapter } from './BcryptAdapter'
 
+const salt = 12
+
 jest.mock('bcrypt', () => ({
   async hash(): Promise<string> {
     return new Promise(resolve => resolve('hash-to-mock'))
   }
 }))
 
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter(salt)
+}
+
 describe('Bcrypt Adapter', () => {
   test('Should call bcrypt correct values', async () => {
-    const salt = 12
-    const sut = new BcryptAdapter(salt)
+    const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
   test('Should return a hash on success', async () => {
-    const salt = 12
-    const sut = new BcryptAdapter(salt)
+    const sut = makeSut()
     const hashCreated = await sut.encrypt('hash-to-mock')
 
     expect(hashCreated).toBe('hash-to-mock')
