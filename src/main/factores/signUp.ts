73e35a107380b6f1cb75a8d8561a266c92a436f5
raulrocha2 <raul.rocha2@gmail.com>
2022-05-03 21:44:32 -0300

    feat: add SignUp factory

---
+++ src/main/factores/signUp.ts
@@ -0,0 +1,17 @@
+import { DbAddAccount } from "../../data/useCases/addAccount/DbAddAccount";
+import { BcryptAdapter } from "../../infra/criptography/BcryptAdapter";
+import { AccountMongoRepository } from "../../infra/db/mongodb/accountRepository/AccountMongoRepository";
+import { SignUpController } from "../../presentation/controllers/signUp/SignUpController";
+import { EmailValidatorAdapter } from "../../utils/EmailValidatorAdapter";
+
+
+export const makeSignUpController = (): SignUpController => {
+  const salt = 20
+
+  const accountMongoRepository = new AccountMongoRepository()
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const addAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
+
+  return new SignUpController(emailValidatorAdapter, addAccount)
+}
