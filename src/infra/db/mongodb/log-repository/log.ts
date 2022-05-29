918ab64f4e337ce4080d0792c36ea47fb9e7f569
raulrocha2 <raul.rocha2@gmail.com>
2022-05-29 17:53:33 -0300

    feat: ensure LogMongoRepository create an error log on success

---
+++ src/infra/db/mongodb/log-repository/log.ts
@@ -0,0 +1,11 @@
+import { ILogErrorRepository } from "../../../../data/protocols/ILogErrorRepository";
+import { MongoHelper } from "../helpers/mongoHelper";
+
+export class LogMongoRepository implements ILogErrorRepository {
+
+  async logError(stack: string): Promise<void> {
+    const errorCollection = await MongoHelper.getCollection('errors')
+    await errorCollection.insertOne({ stack, date: new Date() })
+  }
+
+}
