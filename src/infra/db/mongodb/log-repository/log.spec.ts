c86608a6132201526a69332a9a138b850af7a555
raulrocha2 <raul.rocha2@gmail.com>
2022-05-29 18:19:12 -0300

    refactor: move sut creattion to a factory

--- src/infra/db/mongodb/log-repository/log.spec.ts
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -1,10 +1,15 @@
 import { Collection } from "mongodb"
+import { ILogErrorRepository } from "../../../../data/protocols/ILogErrorRepository"
 import { MongoHelper } from "../helpers/mongoHelper"
 import { LogMongoRepository } from "./log"
 
 describe('Log Mongo Repository', () => {
 
   let errorCollection: Collection
+
+  const makeSut = (): ILogErrorRepository => {
+    return new LogMongoRepository()
+  }
 
   beforeAll(async () => {
     await MongoHelper.connect('mongodb://localhost:27017/test')
@@ -20,7 +25,7 @@ describe('Log Mongo Repository', () => {
   })
 
   test('Should create an error log on success', async () => {
-    const sut = new LogMongoRepository()
+    const sut = makeSut()
     await sut.logError('any_error')
     const count = await errorCollection.countDocuments()
     expect(count).toBe(1)
