55c981074cc6f4969387d7e014e407a299c4b9a2
raulrocha2 <raul.rocha2@gmail.com>
2022-06-09 20:31:41 -0300

    test: ensure LogiController return 400 if invalid email is provided

--- src/presentation/controllers/signUp/login/login.spec.ts
+++ src/presentation/controllers/signUp/login/login.spec.ts
@@ -1,4 +1,4 @@
-import { MissingParamError } from "../../../error";
+import { InvalidParamError, MissingParamError } from "../../../error";
 import { badRequest } from "../../../helpers/httpHelper";
 import { IEmailValidator } from "../signUpProtocols";
 import { LoginController } from "./login"
@@ -39,6 +39,19 @@ describe('Login controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')));
+  })
+
+  test('Should return 400 if an invalid email is provided', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const httpRequest = {
+      body: {
+        email: 'any_email@test.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')));
   })
 
   test('Should return 400 if no password is provided', async () => {
