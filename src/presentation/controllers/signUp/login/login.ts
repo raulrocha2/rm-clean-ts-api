71f0df3b3f674c2499a283571275c084d25f9430
raulrocha2 <raul.rocha2@gmail.com>
2022-06-10 16:48:52 -0300

    feat: ensure LoginController returns 200 if valid credentials are provider

--- src/presentation/controllers/signUp/login/login.ts
+++ src/presentation/controllers/signUp/login/login.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from "../../../error";
-import { badRequest, serverError, unauthorized } from "../../../helpers/httpHelper";
+import { badRequest, ok, serverError, unauthorized } from "../../../helpers/httpHelper";
 import { IHttpRequest, IHttpResponse, IController, IEmailValidator, IAuthentication } from "./LoginProtocols";
 
 
@@ -26,11 +26,13 @@ export class LoginController implements IController {
 
       }
 
-      const token = await this.authentication.auth(email, password)
+      const accessToken = await this.authentication.auth(email, password)
 
-      if (!token) {
+      if (!accessToken) {
         return unauthorized()
       }
+
+      return ok({ accessToken })
     } catch (error) {
       return serverError(error)
     }
