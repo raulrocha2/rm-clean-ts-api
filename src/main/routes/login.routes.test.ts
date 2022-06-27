0505028055ee6badd2e8e7ced4be432c7a6d07c8
raulrocha2 <raul.rocha2@gmail.com>
2022-06-27 20:12:06 -0300

    test: ensure return 401 if email or password incorrect

--- src/main/routes/login.routes.test.ts
+++ src/main/routes/login.routes.test.ts
@@ -50,6 +50,16 @@ describe('POST /login', () => {
         password: '123'
       })
       .expect(200)
+  })
+
+  test('Should return 401 on login', async () => {
+    await request(app)
+      .post('/api/login')
+      .send({
+        email: 'rodrigo.manguinho@email.com',
+        password: '123'
+      })
+      .expect(401)
 
   })
 })
